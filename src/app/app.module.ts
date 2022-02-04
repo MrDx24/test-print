071c2b332d1a2c4c3d902e7a80b91b1fdcc2d11d
Deep Patadiya <deeppatadiya24@gmail.com>
2022-02-04 15:02:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './print/canvas.component';
+import { PrintComponent } from './print/print.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrintComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
